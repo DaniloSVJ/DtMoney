eb6b8e15c99de2f2ce71c9ae1719bd4011ae10d0
Danilo Sampaio Vieira <danilocotatoceu@gmail.com>
2022-03-12 18:46:55 -0300

    Update index.tsx

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import Modal from 'react-modal'
-import {FormEvent, useState, useContext} from 'react'
-import { api } from '../../services/api'
+import {FormEvent, useState} from 'react'
+
 import { useTransactions } from '../../hooks/useTransactions'
 
 import { Container ,TransactionTypeContainer,RadioBox} from './styles'
